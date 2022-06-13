35126c3a8c620f1fde7384ab7b19ece257f38d9b
Aruzo <dawid12391111@gmail.com>
2022-06-13 21:45:48 +0200

    #21 added filters to product

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import CategorySerializer, ProductSerializer
 from .models import Category, Product
 
@@ -14,3 +15,9 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = (
+        "category",
+        "bestseller",
+        "special",
+    )
