7487416c6f511f9f8c9d38a6eb37eaa8bc7687ea
Aruzo <dawid12391111@gmail.com>
2022-06-13 21:28:39 +0200

    #19 created category and product serializers

---
+++ backend/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Category, Product
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "category", "name", "description", "bestseller", "special"]
