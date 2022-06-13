b667d817ad3bce993167794cf4e598c2a0543358
Aruzo <dawid12391111@gmail.com>
2022-06-13 21:28:11 +0200

    #19 created category and product models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    bestseller = models.BooleanField()
+    special = models.BooleanField()
+
+    def __str__(self):
+        return self.name
