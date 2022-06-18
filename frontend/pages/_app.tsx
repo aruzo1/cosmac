6bab6804027c51fa2856a9f987e7edc2100e7953
Aruzo <dawid12391111@gmail.com>
2022-06-18 14:38:40 +0200

    #28 installed axios

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from "next/app";
+import axios from "axios";
 import Bar from "components/Bar";
 import Navbar from "components/Navbar";
 import "styles/globals.css";
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 function Cosmac({ Component, pageProps }: AppProps) {
   return (
