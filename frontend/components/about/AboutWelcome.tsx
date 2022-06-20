ba34d541f4ce31c3abbfd804ee7acf446eb25d40
Aruzo <dawid12391111@gmail.com>
2022-06-20 17:02:15 +0200

    #49 created about us page

---
+++ frontend/components/about/AboutWelcome.tsx
@@ -0,0 +1,22 @@
+function AboutWelcome() {
+  return (
+    <div className="flex flex-col gap-y-4 py-8 text-center">
+      <h1 className="text-4xl font-bold">O Nas</h1>
+      <p className="text-accent-light">
+        Jesteśmy doświadczoną i rzetelną firmą, która specjalizuje się w
+        produktach ogrodzeniowych do tymczasowego zabezpieczania placów budów
+        oraz przestrzeni przemysłowych, drogowych czy imprezowych. Do naszej
+        pracy podchodzimy z pasją i zaangażowaniem.
+      </p>
+      <p className="text-accent-light">
+        Obok sprzedaży, świadczymy także usługi wynajmu ogrodzeń tymczasowych,
+        montażu, dostawy oraz brandingu naszych produktów. Doradzamy,
+        opracowujemy plany zabezpieczeń inwestycji budowlanych i dobieramy do
+        nich optymalne rozwiązania produktowe.
+      </p>
+      <p className="text-accent-light">Zapraszamy do współpracy!</p>
+    </div>
+  );
+}
+
+export default AboutWelcome;
