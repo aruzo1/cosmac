42566e9b313cc12b3220aba622895a016b6193d1
Aruzo <dawid12391111@gmail.com>
2022-06-20 13:39:43 +0200

    #46 added fade animation to product modal

--- frontend/components/category/ProductModal.tsx
+++ frontend/components/category/ProductModal.tsx
@@ -3,6 +3,7 @@ import { XIcon } from "@heroicons/react/outline";
 import type { IProduct } from "types";
 import useModal from "hooks/useModal";
 import Portal from "components/hoc/Portal";
+import Fade from "components/hoc/Fade";
 
 interface Props {
   modal: ReturnType<typeof useModal>;
@@ -12,35 +13,40 @@ interface Props {
 function ProductModal({ modal, product }: Props) {
   return (
     <Portal to="modals">
-      <div
-        className="absolute z-20 inset-0 container"
-        style={{ top: modal.top }}
-      >
-        <div className="grid md:grid-cols-12 gap-4 p-4 rounded-lg bg-white">
-          <div className="relative md:col-span-4 aspect-square border rounded-lg border-brand">
-            <Image
-              src={product.img}
-              alt="produkt"
-              className="rounded-lg"
-              layout="fill"
+      <Fade show={modal.show}>
+        <div
+          className="absolute z-20 inset-0 container"
+          style={{ top: modal.top }}
+        >
+          <div className="grid md:grid-cols-12 gap-4 p-4 rounded-lg bg-white">
+            <div className="relative md:col-span-4 aspect-square border rounded-lg border-brand">
+              <Image
+                src={product.img}
+                alt="produkt"
+                className="rounded-lg"
+                layout="fill"
+              />
+            </div>
+            <div className="md:col-span-7">
+              <h2 className="text-4xl font-bold">{product.name}</h2>
+              <p className="mt-2 text-accent-light">{product.description}</p>
+            </div>
+            <XIcon
+              className="order-first md:order-none w-8 h-8 ml-auto hover:text-accent-light cursor-pointer transition-colors"
+              onClick={modal.close}
+            />
+            <img
+              src={product.table_img}
+              alt="tabela produktu"
+              className="w-full col-span-full"
             />
           </div>
-          <div className="md:col-span-7">
-            <h2 className="text-4xl font-bold">{product.name}</h2>
-            <p className="mt-2 text-accent-light">{product.description}</p>
-          </div>
-          <XIcon
-            className="order-first md:order-none w-8 h-8 ml-auto hover:text-accent-light cursor-pointer transition-colors"
-            onClick={modal.close}
-          />
-          <img
-            src={product.table_img}
-            alt="tabela produktu"
-            className="w-full col-span-full"
-          />
         </div>
-      </div>
-      <div className="fixed z-10 inset-0 bg-accent/50" onClick={modal.close} />
+        <div
+          className="fixed z-10 inset-0 bg-accent/50"
+          onClick={modal.close}
+        />
+      </Fade>
     </Portal>
   );
 }
